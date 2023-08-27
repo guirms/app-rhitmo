dc07c6ecabd8f4dcaa38ebb63b9074fa6501dfa9
guirms <guilhermesantana84@hotmail.com>
2023-08-27 02:30:47 -0300

    feat: validating cities

--- Front-end/test-rhitmo/src/app/pages/customer-registration/customer-registration.component.ts
+++ Front-end/test-rhitmo/src/app/pages/customer-registration/customer-registration.component.ts
@@ -13,7 +13,7 @@ import { environment } from 'src/environments/environment';
 })
 export class CustomerRegistrationComponent implements OnInit {
 
-  cadastroForm!: FormGroup;
+  registerForm!: FormGroup;
   name!: string;
   email!: string;
   cpf!: string;
@@ -22,6 +22,9 @@ export class CustomerRegistrationComponent implements OnInit {
   address!: string;
   state!: string | null;
   cep!: string;
+  city!: string | null;
+  cityDictionary: { [key: number]: string } = {};
+  cityPlaceholder = "Selecione o estado antes";
 
   readonly stateDictionary: { [key: number]: string } = {};
 
@@ -30,21 +33,31 @@ export class CustomerRegistrationComponent implements OnInit {
     public baseService: BaseService,
     public customerService: CustomerService) {
 
-    const stateNames = environment.brazilLocations.States.map(s => s.Name);
+    const stateNames: string[] = environment.brazilLocations.States.map(s => s.Name)
+      .sort();
+
     for (let i = 0; i < stateNames.length; i++) {
       this.stateDictionary[i + 1] = stateNames[i];
+    }
+
+    const cityNames: string[] = environment.brazilLocations.States
+      .flatMap(state => state.Cities)
+      .sort();
+
+    for (let i = 0; i < cityNames.length; i++) {
+      this.cityDictionary[i + 1] = cityNames[i];
     }
   }
 
   ngOnInit(): void {
-    this.cadastroForm = this.formBuilder.group({
+    this.registerForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       cpf: ['', [Validators.required]],
       address: ['', [Validators.required]],
       state: [0, [Validators.required]],
       cep: ['', [Validators.required]],
-      city: ['', [Validators.required]],
+      city: [0, [Validators.required]],
       cardholderName: ['', []],
       cardNumber: ['', []],
       cardExpirationDate: ['', []],
@@ -53,23 +66,22 @@ export class CustomerRegistrationComponent implements OnInit {
   }
 
   registerClient(): void {
-
   }
 
   validateField(fieldName: string): boolean {
-    const formField = this.cadastroForm.get(fieldName);
+    const formField = this.registerForm.get(fieldName);
 
     return (formField?.invalid && (formField?.dirty || formField?.touched) && !formField?.errors?.email) ?? false;
   }
 
   validateEmail(): boolean {
-    const formField = this.cadastroForm.get('email');
+    const formField = this.registerForm.get('email');
 
     return (formField?.errors?.email && (formField?.dirty || formField?.touched)) ?? false;
   }
 
   validateCpf(): boolean {
-    const formField = this.cadastroForm.get('cpf');
+    const formField = this.registerForm.get('cpf');
 
     if (formField) {
       return (this.cpf?.length !== 14 && formField?.touched) ?? false;
@@ -79,13 +91,15 @@ export class CustomerRegistrationComponent implements OnInit {
   }
 
   validateState(): boolean {
-    const formField = this.cadastroForm.get('state');
+    const formField = this.registerForm.get('state');
 
     return (!this.state && formField?.touched) ?? false;
   }
 
-  validateCep(): boolean {
-    return false;
+  validateCity(): boolean {
+    const formField = this.registerForm.get('city');
+
+    return (!this.city && formField?.touched) ?? false;
   }
 
   formatCpf(): void {
@@ -116,6 +130,7 @@ export class CustomerRegistrationComponent implements OnInit {
       this.cep = cleanCep.replace(/(\d{2})(\d{3})(\d{3})/g, '$1.$2 - $3');
     }
   }
+
   addCharactere(text: string, charactere: string, position: number): string {
     const primeiraParte = text.slice(0, position);
     const segundaParte = text.slice(position);
@@ -127,10 +142,58 @@ export class CustomerRegistrationComponent implements OnInit {
     const target = event.target as HTMLSelectElement;
 
     if (Number(target.value) === 0) {
+      this.clearCity();
       this.state = null;
     }
     else {
       this.state = target.value;
+      this.cityDictionary = {};
+
+      const filteredState = environment.brazilLocations.States
+        .filter(s => s.Name === this.stateDictionary[Number(this.state)]);
+
+      if (this.city) {
+        const cityExists = filteredState
+          .some(c => c.Cities.some(c => c === this.cityDictionary[Number(this.city)]));
+
+        if (!cityExists) {
+          this.city = null;
+        }
+      }
+      
+      this.cityDictionary = {};
+
+      const cityNames: string[] = filteredState
+        .flatMap(state => state.Cities)
+        .sort();
+
+      for (let i = 0; i < cityNames.length; i++) {
+        this.cityDictionary[i + 1] = cityNames[i];
+      }
+    }
+  }
+
+  clearCity(): void {
+    this.cityDictionary = {};
+    this.city = null;
+
+    const cityNames: string[] = environment.brazilLocations.States
+      .flatMap(state => state.Cities)
+      .sort();
+
+    for (let i = 0; i < cityNames.length; i++) {
+      this.cityDictionary[i + 1] = cityNames[i];
+    }
+  }
+
+  setCity(event: Event) {
+    const target = event.target as HTMLSelectElement;
+
+    if (Number(target.value) === 0) {
+      this.city = null;
+    }
+    else {
+      this.city = target.value;
     }
   }
 
